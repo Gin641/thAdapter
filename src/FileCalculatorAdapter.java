9fe77b10b911c8a592b6d2ba11ef07efee9bc082
Gin641 <53481351+Gin641@users.noreply.github.com>
2020-04-20 17:53:21 +0700

    kho hieu

---
+++ src/FileCalculatorAdapter.java
@@ -0,0 +1,10 @@
+import java.io.File;
+import com.codegym.FileUtil;
+public class FileCalculatorAdapter implements FileCalculator {
+    @Override
+    public long calculateSize(String path){
+        FileUtil fileUtil = new FileUtil();
+        File file = new File(path);
+        return fileUtil.calculateSize(file);
+    }
+}
